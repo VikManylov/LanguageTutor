8d91950c859a8b4ff0b96ed43dadc7d5dc3b5c43
Viktor <winner1.92@mail.ru>
2022-01-26 01:32:49 +0700

    ДОбавил пакет для работы с телеграмом. Настороил связь с созданым ботом.

--- LanguegeTutor/LanguegeTutor/Program.cs
+++ LanguegeTutor/LanguegeTutor/Program.cs
@@ -1,31 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using Telegram.Bot;
+
+
+
 
 namespace LanguegeTutor
 {
     class Program
     {
+        static TelegramBotClient Bot;
+
         static void Main(string[] args)
         {
-            var tutor = new Tutor();
-            tutor.AddWord("hello", "привет");
-            tutor.AddWord("dog", "собака");
-            tutor.AddWord("cat", "кошка");
-            tutor.AddWord("byke", "велосипед");
+            Bot = new TelegramBotClient("5258298801:AAGGG0IZg8_RRyrNvJMm88jRBDBVozoaThE");
 
-            while(true)
-            {
-                var word = tutor.GetRandomEngWord();
-                Console.WriteLine($"Как переводиться слово: {word}?");
-                var userAnswer = Console.ReadLine();
-                if (tutor.CheckWord(word, userAnswer))
-                    Console.WriteLine("Правильно");
-                else
-                {
-                    var correctAnswer = tutor.Translate(word);
-                    Console.WriteLine($"Неверно. Правильный ответ: {correctAnswer}");
-                }
-            }
+            //var me = Bot.GetMeAsync().Result;
+            //Console.WriteLine(me.FirstName);
+
+            
+            Bot.OnMessage += Bot_OnMessage;
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+
+            //var tutor = new Tutor();
+            //tutor.AddWord("hello", "привет");
+            //tutor.AddWord("dog", "собака");
+            //tutor.AddWord("cat", "кошка");
+            //tutor.AddWord("byke", "велосипед");
+
+            //while(true)
+            //{
+            //    var word = tutor.GetRandomEngWord();
+            //    Console.WriteLine($"Как переводиться слово: {word}?");
+            //    var userAnswer = Console.ReadLine();
+            //    if (tutor.CheckWord(word, userAnswer))
+            //        Console.WriteLine("Правильно");
+            //    else
+            //    {
+            //        var correctAnswer = tutor.Translate(word);
+            //        Console.WriteLine($"Неверно. Правильный ответ: {correctAnswer}");
+            //    }
+            //}
+        }
+
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Console.WriteLine(e.Message.Text);
         }
     }
 }
